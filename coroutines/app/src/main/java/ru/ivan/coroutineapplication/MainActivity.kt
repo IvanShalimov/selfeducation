85a2811fbf566f99244b4ba4c90819d8c88bf70e
Ivan <gogi-mogel@mail.ru>
2021-05-08 20:46:08 +0300

    Finish lesson 8

--- coroutines/app/src/main/java/ru/ivan/coroutineapplication/MainActivity.kt
+++ coroutines/app/src/main/java/ru/ivan/coroutineapplication/MainActivity.kt
@@ -3,18 +3,34 @@ package ru.ivan.coroutineapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.view.View
 import kotlinx.coroutines.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
+/**
+ * Курс по корутинам был взят здесь: https://startandroid.ru/ru/courses/kotlin.html
+ */
 class MainActivity : AppCompatActivity() {
 
-    lateinit var scope:CoroutineScope
+    private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
+    private val scope:CoroutineScope = CoroutineScope(Job())
+    lateinit var cancelableJob: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        scope = CoroutineScope(Job())
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        scope.cancel()
+    }
+
+    /* Обработчик нажатия для кнопки с id="@+id/button" */
+    fun firstTry(view: View) {
 
         scope.launch {
             delay(10000)
@@ -29,19 +45,52 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "first coroutine "+hashCode())
         }
 
-        findViewById<Button>(R.id.button).setOnClickListener {
-            Log.d(TAG, "click")
-        }
-
         val job = scope.launch {
             Log.d(TAG, "scope = $this")
         }
         Log.d(TAG, "job = $job")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        scope.cancel()
+    /* Обработчик нажатия для кнопки с id="@+id/lesson_eight_run" */
+    fun onRun(view:View) {
+        log("onRun, start")
+        scope.launch {
+            log("coroutine start")
+            delay(1000)
+            log("coroutine end")
+        }
+
+        log("onRun, middle")
+
+        scope.launch {
+            log("coroutine2 start")
+            delay(1500)
+            log("coroutine2 end")
+        }
+
+        cancelableJob = scope.launch {
+            log("coroutine3 start")
+            var x = 0
+            while (x < 5 && isActive) {
+                //Из-за этой функции(suspend под капотом проверяет флаг) проверка флага не нужна, нужно для  TimeUnit.MILLISECONDS.sleep(1000)
+                delay(1000)
+                log("coroutine, x = ${x++}, isActive = $isActive")
+            }
+            log("coroutine3 end")
+        }
+
+        log("onRun, end")
+    }
+
+    /* Обработчик нажатия для кнопки с id="@+id/lesson_eight_cancel" */
+    fun onCancel(view:View) {
+        log("onCancel")
+        cancelableJob.cancel()
+    }
+
+    /*Метод с логами для урока # 8*/
+    private fun log(text:String) {
+        Log.d(TAG, "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
     }
 
     companion object {
