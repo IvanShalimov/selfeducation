e982d61d32cf012bbb34364e876bd46da66a87d4
Ivan <gogi-mogel@mail.ru>
2021-05-30 12:43:22 +0300

    Flow example + view binding

--- CoroutineFlowApplication/app/src/main/java/ru/ivan/coroutineflowapplication/MainActivity.kt
+++ CoroutineFlowApplication/app/src/main/java/ru/ivan/coroutineflowapplication/MainActivity.kt
@@ -1,20 +1,30 @@
 package ru.ivan.coroutineflowapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import ru.ivan.coroutineflowapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel =
             ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(
                 MainViewModel::class.java
             )
+
+        binding.actionButton.setOnClickListener {
+            viewModel.count { number ->
+                Log.d("Ivan", "current number is $number")
+            }
+        }
     }
 }
--- CoroutineFlowApplication/app/src/main/java/ru/ivan/coroutineflowapplication/MainViewModel.kt
+++ CoroutineFlowApplication/app/src/main/java/ru/ivan/coroutineflowapplication/MainViewModel.kt
@@ -1,6 +1,32 @@
 package ru.ivan.coroutineflowapplication
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    private var currentJob: Job? = null
+
+    fun count(callback: (Int) -> Unit) {
+        currentJob?.let {
+            if (it.isActive) return
+        }
+
+        currentJob = viewModelScope.launch {
+            getFlowCounter().collect { number ->
+                callback(number)
+            }
+        }
+    }
+
+    private fun getFlowCounter() = flow {
+        for (i in 1..10) {
+            kotlinx.coroutines.delay(1000)
+            emit(i)
+        }
+    }
 }
