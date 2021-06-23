94772444748a9ef13789e46bc0703d63b18bbb8e
Ivan <gogi-mogel@mail.ru>
2021-06-23 17:06:55 +0300

    Init new project

--- CustomView/app/src/test/java/ru/ivan/customview/ExampleUnitTest.kt
+++ CustomView/app/src/test/java/ru/ivan/customview/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
-package ru.ivan.coroutineflowapplication
+package ru.ivan.customview
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,12 +13,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-
-        val a:A? = null
-        val c = a?.b ?: 0
-    }
-
-    class A() {
-        val b = 9
     }
 }
