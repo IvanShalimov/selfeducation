9a1f74d5c1d4ec6fd23b4ed64c095a9ff0d7e261
Ivan <gogi-mogel@mail.ru>
2021-06-23 20:23:44 +0300

    Example work with Path

--- CustomView/app/src/main/java/ru/ivan/customview/MainActivity.kt
+++ CustomView/app/src/main/java/ru/ivan/customview/MainActivity.kt
@@ -2,11 +2,12 @@ package ru.ivan.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.ivan.customview.customviews.DrawableWithStringBuilder
+import ru.ivan.customview.customviews.PathDrawableView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(DrawableWithStringBuilder(this))
+        setContentView(PathDrawableView(this))
     }
 }
---
+++ CustomView/app/src/main/java/ru/ivan/customview/customviews/PathDrawableView.kt
@@ -0,0 +1,66 @@
+package ru.ivan.customview.customviews
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class PathDrawableView : View {
+
+
+    private val paint = Paint()
+    private val rectf = RectF(350f, 100f, 400f, 150f)
+    private val path = Path()
+    private val path1 = Path()
+
+    //Необходимые для View в layout конструкторы.
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.drawARGB(80, 102, 204, 255)
+
+        //Очистка path
+        path.reset()
+
+        //Угол
+        path.moveTo(100f, 100f)
+        path.lineTo(150f, 200f)
+        path.lineTo(50f, 200f)
+
+        //Треугольник
+        path.moveTo(250f, 100f)
+        path.lineTo(300f, 200f)
+        path.lineTo(200f, 200f)
+
+        path.close()
+
+        //Квадрат и круг
+        path.addRect(rectf, Path.Direction.CW)
+        path.addCircle(450f, 150f, 25f, Path.Direction.CW)
+
+        paint.color = Color.BLACK
+        canvas?.drawPath(path, paint)
+
+        path1.reset()
+        path1.moveTo(50f,50f)
+        path1.lineTo(500f, 250f)
+        path1.moveTo(500f, 50f)
+        path1.lineTo(50f, 250f)
+
+        paint.color = Color.GREEN
+        canvas?.drawPath(path1, paint)
+
+        path.addPath(path1)
+
+        path.offset(500f,100f)
+
+        paint.color = Color.BLUE
+        canvas?.drawPath(path, paint)
+    }
+}
