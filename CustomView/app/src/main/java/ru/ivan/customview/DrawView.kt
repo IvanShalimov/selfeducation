07908535e71bb4add9879c2a49e976035e042302
Ivan <gogi-mogel@mail.ru>
2021-06-23 17:21:52 +0300

    Add support for layout

--- CustomView/app/src/main/java/ru/ivan/customview/DrawView.kt
+++ CustomView/app/src/main/java/ru/ivan/customview/DrawView.kt
@@ -3,9 +3,14 @@ package ru.ivan.customview
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.util.AttributeSet
 import android.view.View
 
-class DrawView(context: Context): View(context) {
+class DrawView: View {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context,  attrs: AttributeSet?):this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.drawColor(Color.GREEN)
